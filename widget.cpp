ac9fd5bef95f87d0e9301f669f06d94201fd91d2
hanrai <hanrai@gmail.com>
2014-07-12 21:07:43 +0800

    Delete an empty line

--- widget.cpp
+++ widget.cpp
@@ -52,7 +52,6 @@ QString decode(QByteArray &data)
     quint32 blocks = ntohl(*(quint32*)cursor);
     cursor+=4;
 
-
     quint32 datetime1 = *(quint32*)cursor;
     cursor+=4;
     quint32 datetime2 = *(quint32*)cursor;
